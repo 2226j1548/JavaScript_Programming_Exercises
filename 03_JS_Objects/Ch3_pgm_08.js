ef42b5ff78bf111ee952278d53592e7b1e5020fd
2226j1548 <144645767+2226j1548@users.noreply.github.com>
2024-07-14 19:30:12 +0530

    Update Ch3_pgm_08.js

--- 03_JS_Objects/Ch3_pgm_08.js
+++ 03_JS_Objects/Ch3_pgm_08.js
@@ -10,7 +10,17 @@ book = {
 
 console.log(book.title);
 console.log(book.author);
+console.log(book.published);
 
+book2 = {
+  title  : "The Hobbit",
+  author : "J.R.R.Tolkien",
+  published : 1937
+};
+
+console.log(book2.title);
+console.log(book2.author);
+console.log(book2.published);
 
 
 /* Further Adventures
