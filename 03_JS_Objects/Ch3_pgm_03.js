e87000b3b247272f2894d46e47c3607facec5c29
2226j1548 <144645767+2226j1548@users.noreply.github.com>
2024-07-14 17:09:23 +0530

    Update Ch3_pgm_03.js

--- 03_JS_Objects/Ch3_pgm_03.js
+++ 03_JS_Objects/Ch3_pgm_03.js
@@ -5,10 +5,18 @@ var book;
 book={
     booktitle : "Ikigai",
     bookauthor:"Hector Garcia",
-    published:"2016",
+    published:"2016"
    };
    console.log(book);
 
+var book2;
+
+book2={
+    booktitle : "Harry Potter and the Philosopher's Stone",
+    bookauthor:"J. K. Rowling",
+    published:"26 June 1997"
+   };
+   console.log(book2);
 
 
 /* Further Adventures
