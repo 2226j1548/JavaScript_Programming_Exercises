2de50c6ae00e8000d75f5c2b784a6573b0a2f582
2226j1548 <144645767+2226j1548@users.noreply.github.com>
2024-07-14 13:58:23 +0530

    Update Ch3_pgm_10.js

--- 03_JS_Objects/Ch3_pgm_10.js
+++ 03_JS_Objects/Ch3_pgm_10.js
@@ -1,17 +1,16 @@
 // Using dot notation to update a property
 
 var player1;
-
 player1 = {
     name: "Max",
     attempted: 0,
     correct: 0
 };
-
-player1.attempted = 1;
-player1.correct = 1;
-player1.score = 50;
-
+player1.name = "Max";
+player1.attempted = 5;
+player1.correct = 3;
+player1.score = 100;
+console.log(player1);
 
 
 /* Further Adventures
