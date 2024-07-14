ce5b0ccc88ac414d5e36c138facd5c2eb052ee6c
2226j1548 <144645767+2226j1548@users.noreply.github.com>
2024-07-14 17:03:44 +0530

    Update Ch3_pgm_02.js

--- 03_JS_Objects/Ch3_pgm_02.js
+++ 03_JS_Objects/Ch3_pgm_02.js
@@ -2,18 +2,18 @@
 
 var book1Title = "Northern Lights";
 var book1Author = "Philip Pullman";
-
 var book2Title = "The Hobbit";
 var book2Author = "J.R.R.Tolkien";
-
 var book3Title = "The Adventures of Tom Sawyer";
 var book3Author = "Mark Twain";
+var book4Title = "To Kill a Mockingbird";
+var book4Author = "Harper Lee";
 
 console.log("There are three books so far...");
 console.log(book1Title + " by " + book1Author);
 console.log(book2Title + " by " + book2Author);
 console.log(book3Title + " by " + book3Author);
-
+console.log(book4Title + " by " + book4Author);
 
 
 /* Further Adventures
